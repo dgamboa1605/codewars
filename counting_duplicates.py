923a06d0e49301eae83446417277b1f8ca7719fe
dennis gamboa <dgamboa@gmail.com>
2023-08-07 12:36:43 -0400

    counting duplicates in string

---
+++ counting_duplicates.py
@@ -0,0 +1,13 @@
+def duplicate_count(text):
+    text = text.lower()
+    letters = [x for x in text]
+    return len(set([x for x in letters if letters.count(x) > 1]))
+
+
+print(duplicate_count("abcde"))
+print(duplicate_count("aabbcde"))
+print(duplicate_count("aabBcde"))
+print(duplicate_count("indivisibility"))
+print(duplicate_count("Indivisibilities"))
+print(duplicate_count("aA11"))
+print(duplicate_count("ABBA"))
